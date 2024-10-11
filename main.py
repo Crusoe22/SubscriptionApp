bd563a64e6f8dd4a685b6cba829050dcda83d1f0
Crusoe22 <nolanmmoss@gmail.com>
2024-10-11 10:44:22 -0400

    updated kv formatting

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from kivy.app import App
+'''from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from kivy.uix.screenmanager import Screen
+from kivy.uix.gridlayout import GridLayout
 
 class HomeScreen(Screen):
     pass
@@ -14,18 +14,68 @@ class SettingsScreen(Screen):
 class LoginScreen(Screen):
     pass
 
-#Enter Username and password
-class PasswordScreen(GridLayout):
-
+class PasswordScreen(Screen):
     def __init__(self, **kwargs):
         super(PasswordScreen, self).__init__(**kwargs)
-        self.cols = 2
-        self.add_widget(Label(text='User Name'))
+        layout = GridLayout(cols=2)
+        layout.add_widget(Label(text='User Name'))
         self.username = TextInput(multiline=False)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='password'))
+        layout.add_widget(self.username)
+        layout.add_widget(Label(text='Password'))
         self.password = TextInput(password=True, multiline=False)
-        self.add_widget(self.password)
+        layout.add_widget(self.password)
+        self.add_widget(layout)
+
+# Load the KV file
+GUI = Builder.load_file("main.kv")
+
+class MyApp(App):
+    def build(self):
+        return GUI
+
+    def change_screen(self, screen_name):
+        # Get the existing ScreenManager
+        screen_manager = self.root.ids["screen_manager"]
+        
+        # Set the desired transition
+        screen_manager.transition = WipeTransition()
+        
+        # Change the current screen
+        screen_manager.current = screen_name
+
+if __name__ == '__main__':
+    MyApp().run()
+'''
+
+
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
+
+class HomeScreen(Screen):
+    pass
+
+class SettingsScreen(Screen):
+    pass
+
+class LoginScreen(Screen):
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        layout = GridLayout(cols=2)
+        layout.add_widget(Label())#(text='User Name'))
+        self.username = TextInput(multiline=False)
+        layout.add_widget(self.username)
+        layout.add_widget(Label())#(text='Password'))
+        self.password = TextInput(password=True, multiline=False)
+        layout.add_widget(self.password)
+        self.add_widget(layout)
+
+class RegisterScreen(Screen):
+    pass
 
 GUI = Builder.load_file("main.kv")
 
@@ -36,8 +86,13 @@ class MyApp(App):
 
     def change_screen(self, screen_name):
         # Get Screen_manager from kv file
-        #print(self.root.ids)
+        # Get the existing ScreenManager
         screen_manager = self.root.ids["screen_manager"]
+        
+        # Set the desired transition
+        screen_manager.transition = WipeTransition()
+        
+        # Change the current screen
         screen_manager.current = screen_name
 
 if __name__ == '__main__':
